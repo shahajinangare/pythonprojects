9ba924adc79198f7053212d735446af4cfece927
myushahaji <shahaji.nangare@adityabirla.com>
2018-01-31 18:48:10 +0530

    checked in empapi

---
+++ empapi/main.py
@@ -0,0 +1,70 @@
+import os
+from app import app, db
+
+from flask_restful import Resource, Api
+from sqlalchemy import create_engine
+from json import dumps
+
+api = Api(app)
+
+class Employees(Resource):
+    def get(self):
+        conn = db_connect.connect()  # connect to database
+        query = conn.execute("select * from employees")  # This line performs query and returns json result
+        return {'employees': [i[0] for i in query.cursor.fetchall()]}  # Fetches first column that is Employee ID
+
+    def post(self):
+        conn = db_connect.connect()
+        print(request.json)
+        LastName = request.json['LastName']
+        FirstName = request.json['FirstName']
+        Title = request.json['Title']
+        ReportsTo = request.json['ReportsTo']
+        BirthDate = request.json['BirthDate']
+        HireDate = request.json['HireDate']
+        Address = request.json['Address']
+        City = request.json['City']
+        State = request.json['State']
+        Country = request.json['Country']
+        PostalCode = request.json['PostalCode']
+        Phone = request.json['Phone']
+        Fax = request.json['Fax']
+        Email = request.json['Email']
+        query = conn.execute("insert into employees values(null,'{0}','{1}','{2}','{3}', \
+                             '{4}','{5}','{6}','{7}','{8}','{9}','{10}','{11}','{12}', \
+                             '{13}')".format(LastName, FirstName, Title,
+                                             ReportsTo, BirthDate, HireDate, Address,
+                                             City, State, Country, PostalCode, Phone, Fax,
+                                             Email))
+        return {'status': 'success'}
+
+class Tracks(Resource):
+    def get(self):
+        conn = db_connect.connect()
+        query = conn.execute("select trackid, name, composer, unitprice from tracks;")
+        result = {'data': [dict(zip(tuple(query.keys()), i)) for i in query.cursor]}
+        return jsonify(result)
+
+class Employees_Name(Resource):
+    def get(self, employee_id):
+        conn = db_connect.connect()
+        query = conn.execute("select * from employees where EmployeeId =%d " % int(employee_id))
+        result = {'data': [dict(zip(tuple(query.keys()), i)) for i in query.cursor]}
+        return jsonify(result)
+
+api.add_resource(Employees, '/employees')  # Route_1
+api.add_resource(Tracks, '/tracks')  # Route_2
+api.add_resource(Employees_Name, '/employees/<employee_id>')  # Route_3
+
+
+'''
+http://localhost:5011/employees
+http://localhost:5011/tracks
+http://localhost:5011/employees/8 
+'''
+
+if __name__ == '__main__':
+
+    # Build a sample db on the fly, if one does not exist yet.
+
+     app.run('localhost',5011)
